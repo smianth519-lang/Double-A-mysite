bc8f1b37dbcc88be1b2cf1e841bd1bc7bf2e50ae
smianth519-lang <smianth519@gmail.com>
2025-08-29 17:03:08 -0500

    Update doublea.php

--- doublea.php
+++ doublea.php
@@ -1,19 +1,9 @@
  <?php
    
-        $name = htmlspecialchars($_POST['name']);
-        $email = htmlspecialchars($_POST['email']);
-        $number = htmlspecialchars($_POST['number']);
-        $subject = htmlspecialchars($_POST['text']);
-        $message = htmlspecialchars($_POST['message']);
-
-       $mailheader = "From:" .$name. "" ."". $email ."". $number ."". $subject .">\r\n";
-
-        $recipient = "doubleawebdesigning@gmail.com";
-
-        if (mail($recipient, $subject, $message, $mailheader)) {
+       
             echo "message sent";
-        } else {
-            die("Error!");
-        }
+         
+            
     
     ?>
+
